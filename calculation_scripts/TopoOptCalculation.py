Fixed a bug in plotting average penalty, cleaned up code, and finished PR changes. Ready to merge

calculation_scripts/TopoOptCalculation.py
@@ -37,24 +37,25 @@ def _saveAveragePenalty(avg_penalties, path):
 def _savePenaltyShape(penalty_type, path):
     curr_path = os.path.join(path, "Penalty_Shape.txt")
     x_values = np.linspace(0, 1, 200)
-    if penalty_type == 'parabolic':
-        table = np.column_stack((x_values, binarizers.parabolic(x_values, penalty_config)))
-        np.savetxt(curr_path, table, delimiter=',')
-        curr_path = os.path.join(path, "PenaltyGradient_Shape.txt")
-        table = np.column_stack((x_values, binarizers.gradParabolic(x_values, penalty_config)))
-        np.savetxt(curr_path, table, delimiter=',')
-    elif penalty_type == 'gaussian':
-        table = np.column_stack((x_values, binarizers.gaussian(x_values, penalty_config)))
-        np.savetxt(curr_path, table, delimiter=',')
-        curr_path = os.path.join(path, "PenaltyGradient_Shape.txt")
-        table = np.column_stack((x_values, binarizers.gradGaussian(x_values, penalty_config)))
-        np.savetxt(curr_path, table, delimiter=',')
-    elif penalty_type == 'triangular':
-        table = np.column_stack((x_values, binarizers.triangular(x_values, penalty_config)))
-        np.savetxt(curr_path, table, delimiter=',')
-        curr_path = os.path.join(path, "PenaltyGradient_Shape.txt")
-        table = np.column_stack((x_values, binarizers.gradTriangular(x_values, penalty_config)))
-        np.savetxt(curr_path, table, delimiter=',')
+    match penalty_type:
+        case 'parabolic':
+            table = np.column_stack((x_values, binarizers.parabolic(x_values, penalty_config)))
+            np.savetxt(curr_path, table, delimiter=',')
+            curr_path = os.path.join(path, "PenaltyGradient_Shape.txt")
+            table = np.column_stack((x_values, binarizers.gradParabolic(x_values, penalty_config)))
+            np.savetxt(curr_path, table, delimiter=',')
+        case 'gaussian':
+            table = np.column_stack((x_values, binarizers.gaussian(x_values, penalty_config)))
+            np.savetxt(curr_path, table, delimiter=',')
+            curr_path = os.path.join(path, "PenaltyGradient_Shape.txt")
+            table = np.column_stack((x_values, binarizers.gradGaussian(x_values, penalty_config)))
+            np.savetxt(curr_path, table, delimiter=',')
+        case 'triangular':
+            table = np.column_stack((x_values, binarizers.triangular(x_values, penalty_config)))
+            np.savetxt(curr_path, table, delimiter=',')
+            curr_path = os.path.join(path, "PenaltyGradient_Shape.txt")
+            table = np.column_stack((x_values, binarizers.gradTriangular(x_values, penalty_config)))
+            np.savetxt(curr_path, table, delimiter=',')
 
 def _saveCurrentStructure(all_parameters, path, iteration):
     curr_path = os.path.join(path, "Structures", f"Structure{iteration}.txt")
@@ -77,53 +78,47 @@ def _savePenaltyGradients(penalty_gradients, path, iteration):
     np.savetxt(curr_path, penalty_gradients, delimiter='\n')
 
 def _createDirectories(path):
-    save_dict = {
-            'Structures': save_structures,
-            'Parameters': save_params,
-            'Penalties': save_penalties,
-            'Gradient_Penalties': save_gradients_penalty,
-            'E-Fields': save_fields,
-        }
-
-    for directory, flag in save_dict.items():
-        if flag:
-            Path(os.path.join(path, directory)).mkdir(parents=True, exist_ok=True)
+    directories = ['Structures', 'Parameters', 'Penalties', 'Gradient_Penalties', 'E-Fields']
+    for directory in directories:
+        Path(os.path.join(path, directory)).mkdir(parents=True, exist_ok=True)
 
 def _calculatePenaltyGradients(parameters, penalty_type):
-
-    if penalty_type == 'parabolic':
-        return binarizers.gradParabolic(parameters, penalty_config)
-    elif penalty_type == 'gaussian':
-        return binarizers.gradGaussian(parameters, penalty_config)
-    elif penalty_type == 'triangular':
-        return binarizers.gradTriangular(parameters, penalty_config)
+    match penalty_type:
+        case 'parabolic':
+            return binarizers.gradParabolic(parameters, penalty_config)
+        case 'gaussian':
+            return binarizers.gradGaussian(parameters, penalty_config)
+        case 'triangular':
+            return binarizers.gradTriangular(parameters, penalty_config)
     
 def _calculatePenalty(parameters, penalty_type):
-
-    if penalty_type == 'parabolic':
-        return binarizers.parabolic(parameters, penalty_config)
-    elif penalty_type == 'gaussian':
-        return binarizers.gaussian(parameters, penalty_config)
-    elif penalty_type == 'triangular':
-        return binarizers.triangular(parameters, penalty_config)
+    match penalty_type:
+        case 'parabolic':
+            return binarizers.parabolic(parameters, penalty_config)
+        case 'gaussian':
+            return binarizers.gaussian(parameters, penalty_config)
+        case 'triangular':
+            return binarizers.triangular(parameters, penalty_config)
 
 def _calculatePenaltyCoefficient(iteration, coeff_type):
+    match coeff_type:
+        case 'piecewise':
+            return binarizers.piecewise_update(iteration, coeff_config)
+        case 'piecewise_absolute':
+            return binarizers.piecewise_update_absolute(iteration, coeff_config)
+        case 'linear':
+            return binarizers.linear_update(iteration, coeff_config)
+        case 'exp':
+            return binarizers.exp_update(iteration, coeff_config)
+        case 'constant':
+            return coeff_config["coeff_const"]
 
-    if coeff_type == 'piecewise':
-        return binarizers.piecewise_update(iteration, coeff_config)
-    if coeff_type == 'piecewise_absolute':
-        return binarizers.piecewise_update_absolute(iteration, coeff_config)
-    elif coeff_type == 'linear':
-        return binarizers.linear_update(iteration, coeff_config)
-    elif coeff_type == 'exp':
-        return binarizers.exp_update(iteration, coeff_config)
-    elif coeff_type == 'constant':
-        return coeff_config["coeff_const"]
+def _saveStepSizes(all_step_sizes, path):
+    curr_path = os.path.join(path, "stepsizes.txt")
+    np.savetxt(curr_path, all_step_sizes, delimiter='\n')
 
 # CODE STARTS HERE!!!
-print(f"the argument passed into calcultion script is: {sys.argv[1]} ")
 json_file = sys.argv[1]
-
 with open(json_file) as user_file:
     parsed_json = json.load(user_file)
 
@@ -136,17 +131,6 @@ wavelength = parsed_json["wavelength"]
 initialization = np.loadtxt("initializations/halfcylinder.txt")
 #initialization += np.random.uniform(0, 10e-3, size=initialization.shape)
 dielectric_constants = [1.01 + 0j, 5.96282 + 3.80423e-7j]
-
-# flags for saving different values from calculation
-save_objective = parsed_json["save_objective"]
-save_penalty_coefficients = parsed_json["save_penalty_coefficients"]
-save_penalty_shape = parsed_json["save_penalty_shape"]
-save_average_penalty = parsed_json["save_average_penalty"]
-save_structures = parsed_json["save_structures"]
-save_fields = parsed_json["save_fields"]
-save_gradients_penalty = parsed_json["save_gradients_penalty"]
-save_params = parsed_json["save_params"]
-save_penalties = parsed_json["save_penalties"]
 base_path = parsed_json["base_path"]
 
 #stepArray = np.logspace(-2, 0, 20)
@@ -155,7 +139,7 @@ base_path = parsed_json["base_path"]
 
 #penaltyList = ['parabolic', 'gaussian', 'triangular']
 penalty_type = parsed_json["penalty_type"]
-#coeffList = ['constant', 'linear', 'exp', 'piecewise', 'piecewise_absolute]
+#coeffList = ['constant', 'linear', 'exp', 'piecewise', 'piecewise_absolute']
 coeff_type = parsed_json["coeff_type"]
 
 step_size = parsed_json["step_size"]
@@ -163,19 +147,19 @@ evo_max_iter = parsed_json["evo_max_iteration"]
 
 penalty_config = parsed_json["penalty_configs"][penalty_type]
 coeff_config = parsed_json["coeff_configs"][coeff_type]
-#SLOPE = penalty_config["slope"]
         
-full_path = base_path + f"TestHalfCylinder_it{evo_max_iter}_eps{step_size}_{penalty_type}Penalty_{coeff_type}Coeff0.1"
+full_path = base_path + f"Verify2HalfCylinder_it{evo_max_iter}_eps{step_size}_{penalty_type}Penalty_{coeff_type}Coeff"
 print("Saving value to path: " + full_path)
 data_path = os.path.join(full_path, "Data")
 _createDirectories(data_path)
 
 model = _constructModel()
 optimizer = optimizers.AdamOptimizer()
-# step_size = 0.01 # This works better
+# step_size = 0.01 This works better
 all_objective_values = []
 all_coefficients = []
 avg_penalties = []
+all_step_sizes = []
 
 # main iteration loop for gradient descent optimization
 for iteration in range(evo_max_iter):
@@ -184,51 +168,51 @@ for iteration in range(evo_max_iter):
     objective_value = model.objective()
     print("Objective Value is: " + str(objective_value))
     all_objective_values.append(objective_value) 
-
     obj_gradients = model.gradients(objective_value)
-    
-    if save_structures:
-        all_parameters = model.allParameters()
-        _saveCurrentStructure(all_parameters, data_path, iteration)
-    if save_fields:
-        electric_field = model.getElectricField()
-        _saveCurrentEField(electric_field, data_path, iteration)
+
+    # These are only used for plotting, can be reproduced from 'params'
+    all_parameters = model.allParameters()
+    _saveCurrentStructure(all_parameters, data_path, iteration)
+    electric_field = model.getElectricField()
+    _saveCurrentEField(electric_field, data_path, iteration)
 
     params = model.parameters
+    _saveAllParams(params, data_path, iteration)  
     '''
     v = get_parameters()
     v.reshape([2, -1], order='???')
     v = np.convolve2d(filter, v)
     set_parameters(v.flatten())
-    '''
-    if save_params:
-        _saveAllParams(params, data_path, iteration)           #temporary function for debugging gaussian
+    '''         
     penalty_gradients = _calculatePenaltyGradients(params, penalty_type)
-    if save_gradients_penalty:
-        _savePenaltyGradients(penalty_gradients, data_path, iteration) #also temporary but could keep.
+    _savePenaltyGradients(penalty_gradients, data_path, iteration) 
     penalty = _calculatePenalty(params, penalty_type)
-    if save_penalties:
-        _saveAllPenalties(penalty, data_path, iteration)        #temporary function for debugging gaussian
+    _saveAllPenalties(penalty, data_path, iteration)    
     avg_penalties.append(np.average(penalty))
+
     coeff = _calculatePenaltyCoefficient(iteration, coeff_type)
     #coeff = 0.1
     all_coefficients.append(coeff)
 
-    gradients = obj_gradients - coeff*penalty_gradients
+    gradients = (1-coeff)*obj_gradients - coeff*penalty_gradients
+    #gradients = obj_gradients - coeff*penalty_gradients
     gradients_final = optimizer(gradients)
-
+    
+    # uncomment if you want a variable step size
+    '''
+    if (iteration+1) % 100 == 0:
+        step_size /= 2
+    '''
+    all_step_sizes.append(step_size)
     step = step_size * gradients_final
     new_params = np.clip(params + step, 0, 1)
     model.parameters = new_params
 
-if save_objective:
-    _saveObjective(all_objective_values, data_path)
-if save_penalty_coefficients:
-    _savePenaltyCoefficients(all_coefficients, data_path)
-if save_average_penalty:
-    _saveAveragePenalty(avg_penalties, data_path)
-if save_penalty_shape:
-    _savePenaltyShape(penalty_type, data_path)
+_saveObjective(all_objective_values, data_path)
+_savePenaltyCoefficients(all_coefficients, data_path)
+_saveAveragePenalty(avg_penalties, data_path)
+_savePenaltyShape(penalty_type, data_path)
+_saveStepSizes(all_step_sizes, data_path)
 
 parsed_json["full_path"] = full_path
 

calculation_scripts/plotScript.py
@@ -6,6 +6,7 @@ import itertools
 import os
 import sys
 
+#TODO: Duplicate method from __init__.py in dda_model. Need to make that one accessible
 def _generateGeometry(num_x: int, num_y: int, num_z: int):
     # Geometry is [x y z], major along the first dimension: 0 0 0, 1 0 0, 
     # 2 0 0 ... num_x 0 0, 0 1 0, 1 1 0, ... 0 num_y 0 etc.
@@ -26,16 +27,15 @@ def _plotStructures(geometry, it_start, it_end, num_skip, data_path, plot_path, 
         if i % num_skip == 0:
             print(f'Plotting the {i}th structure')
             diel=np.genfromtxt(os.path.join(data_path, "Structures", f"Structure{i}.txt"))
+            plotting.plotGeometry(geometry, diel, pixel_size, iteration=i, position=plot_path, FullLattice=full_lattice)
 
-            plotting.Shape(geometry, diel, pixel_size, iteration=i, position=plot_path, FullLattice=full_lattice)
-
-def _plotFields(geometry, it_start, it_end, num_skip, data_path, plot_path):
-    E_limit = True
-    E_limit_low = 0
-    E_limit_high = 16
-    x_slice = 11
-    y_slice = 11
-    z_slice = 9
+def _plotFields(geometry, it_start, it_end, num_skip, data_path, plot_path, config):
+    E_limit = config["E_limit"]
+    E_limit_low = config["E_limit_low"]
+    E_limit_high = config["E_limit_high"]
+    x_slice = config["x_slice"]
+    y_slice = config["y_slice"]
+    z_slice = config["z_slice"]
     for i in range(it_start, it_end):
         if i % num_skip == 0:
             print(f'Plotting the {i}th E-Field')
@@ -55,24 +55,18 @@ def _plotFields(geometry, it_start, it_end, num_skip, data_path, plot_path):
                 plotting.EField(geometry, light_direction, light_polarization, E_total, pixel_size, iteration=i, position=plot_path)
 
 def _plotPenalties(it_start, it_end, num_skip, data_path, plot_path):
+    x, penalty_shape=np.loadtxt(os.path.join(data_path, "Penalty_Shape.txt"), delimiter=',', unpack=True)
     for i in range(it_start, it_end):
         if i % num_skip == 0:
             print(f'Plotting the {i}th Penalty Plot')
-            plotting.plotPenalties(data_path, plot_path, i)
+            plotting.plotPenalties(x, penalty_shape, data_path, plot_path, i)
 
 def _plotPenaltyGradients(it_start, it_end, num_skip, data_path, plot_path):
+    x, penalty_gradient_shape=np.loadtxt(os.path.join(data_path, "PenaltyGradient_Shape.txt"), delimiter=',', unpack=True)
     for i in range(it_start, it_end):
         if i % num_skip == 0:
             print(f'Plotting the {i}th Gradient Plot (from Penalty)')
-            plotting.plotPenaltyGradients(data_path, plot_path, i)
-
-def _plotPenaltyShapes(penalty_type, data_path, plot_path):
-    if penalty_type == 'parabolic':
-        return plotting.plotParabolic(data_path, plot_path, penalty_config)
-    elif penalty_type == 'gaussian':
-        return plotting.plotGaussian(data_path, plot_path, penalty_config)
-    elif penalty_type == 'triangular':
-        return plotting.plotTriangular(data_path, plot_path, penalty_config)
+            plotting.plotPenaltyGradients(x, penalty_gradient_shape, data_path, plot_path, i)
     
 def _createDirectories(path):
     plot_dict = {
@@ -109,7 +103,6 @@ evo_max_iter = parsed_json["evo_max_iteration"]
 
 penalty_type = parsed_json["penalty_type"]
 penalty_config = parsed_json["penalty_configs"][penalty_type]
-coeff_type = parsed_json["coeff_type"]
 
 # plotting flags
 plot_structures = parsed_json["plot_structures"]
@@ -128,32 +121,24 @@ plot_penalties = parsed_json["plot_penalties"]
 
 _createDirectories(plot_path)
 geometry = _generateGeometry(geometry_shape[0], geometry_shape[1], geometry_shape[2])
-it_start = 0
-it_end = evo_max_iter
-num_skip = 1
+it_start = parsed_json["it_start"]
+it_end = parsed_json["it_end"]
+num_skip = parsed_json["num_skip"]
 
-_plotPenaltyShapes(penalty_type, data_path, plot_path)
+#_plotPenaltyShapes(penalty_type, data_path, plot_path)
+plotting.plotPenaltyShape(penalty_type, data_path, plot_path, penalty_config)
+plotting.plotStepSizes(evo_max_iter, data_path, plot_path)
+plotting.plotObjectiveFunction(evo_max_iter, data_path, full_path)
+plotting.plotPenaltyCoefficients(evo_max_iter, data_path, plot_path)
+plotting.plotAveragePenalty(evo_max_iter, data_path, plot_path)
 
-if Path(os.path.join(data_path, 'Obj.txt')).exists():
-    plotting.plotObjectiveFunction(evo_max_iter, data_path, full_path)
-
-if Path(os.path.join(data_path, 'Coeffs.txt')).exists():
-    plotting.plotPenaltyCoefficients(evo_max_iter, data_path, plot_path)
-
-if Path(os.path.join(data_path, 'Penalties.txt')).exists():
-    plotting.plotAveragePenalty(evo_max_iter, data_path, plot_path)
-
-if Path(os.path.join(data_path, 'Structures')).exists() and plot_structures:
+if plot_structures:
     _plotStructures(geometry, it_start, it_end, num_skip, data_path, plot_path)
-
 if plot_solid_structures:
     _plotStructures(geometry, it_start, it_end, num_skip, data_path, plot_path, full_lattice=True)
-
-if Path(os.path.join(data_path, 'E-Fields')).exists() and plot_fields:
-    _plotFields(geometry, it_start, it_end, num_skip, data_path, plot_path)
-
-if Path(os.path.join(data_path, 'Penalties')).exists() and plot_penalties:
+if plot_fields:
+    _plotFields(geometry, it_start, it_end, num_skip, data_path, plot_path, parsed_json["EField_config"])
+if plot_penalties:
     _plotPenalties(it_start, it_end, num_skip, data_path, plot_path)
-
-if Path(os.path.join(data_path, 'Gradients_Penalty')).exists() and plot_gradients_penalty:
+if plot_gradients_penalty:
     _plotPenaltyGradients(it_start, it_end, num_skip, data_path, plot_path)

calculation_scripts/plotting.py
@@ -19,6 +19,19 @@ def plotObjectiveFunction(max_iterations, data_path, full_path):
     plt.savefig(os.path.join(full_path, "obj.png"), bbox_inches='tight')
     plt.close()
 
+def plotStepSizes(max_iterations, data_path, plot_path):
+    stepsizes = np.loadtxt(os.path.join(data_path, "stepsizes.txt"))
+    iterations = np.arange(max_iterations)
+    plt.plot(iterations, stepsizes)
+    #plt.legend(loc='lower right')
+    plt.title('Step Size for each Iteration of Calculation')
+    plt.ylabel('Step Size')
+    plt.xlabel('Iteration #')
+    plt.rc('axes', titlesize=14)     # fontsize of the axes title
+    plt.rc('axes', labelsize=12)    # fontsize of the x and y labels
+    plt.savefig(os.path.join(plot_path, "stepsizes.png"), bbox_inches='tight')
+    plt.close()
+
 def plotPenaltyCoefficients(max_iterations, data_path, plot_path):
     coeffs=np.loadtxt(os.path.join(data_path, "Coeffs.txt"))
     iterations = np.arange(max_iterations)
@@ -33,7 +46,7 @@ def plotPenaltyCoefficients(max_iterations, data_path, plot_path):
     plt.close()
 
 def plotAveragePenalty(max_iterations, data_path, plot_path):
-    penalties=np.loadtxt(os.path.join(data_path, "Penalties.txt"))
+    penalties=np.loadtxt(os.path.join(data_path, "Average_Penalties.txt"))
     iterations = np.arange(max_iterations)
     plt.plot(iterations, penalties)
     #plt.legend(loc='lower right')
@@ -45,8 +58,7 @@ def plotAveragePenalty(max_iterations, data_path, plot_path):
     plt.savefig(os.path.join(plot_path, "average_penalty.png"), bbox_inches='tight')
     plt.close()
 
-def plotPenalties(data_path, plot_path, iteration):
-    x, penalty_shape=np.loadtxt(os.path.join(data_path, "Penalty_Shape.txt"), delimiter=',', unpack=True)
+def plotPenalties(x, penalty_shape, data_path, plot_path, iteration):
     params=np.loadtxt(os.path.join(data_path, "Parameters", f"Param{iteration}.txt"))
     penalties = np.loadtxt(os.path.join(data_path, "Penalties", f"Penalty{iteration}.txt"))
     plt.plot(x, penalty_shape)
@@ -60,14 +72,13 @@ def plotPenalties(data_path, plot_path, iteration):
     plt.savefig(os.path.join(plot_path, "Penalties", f"penalty{iteration}.png"), bbox_inches='tight')
     plt.close()
 
-def plotPenaltyGradients(data_path, plot_path, iteration):
-    x, penalty_gradient_shape=np.loadtxt(os.path.join(data_path, "PenaltyGradient_Shape.txt"), delimiter=',', unpack=True)
+def plotPenaltyGradients(x, penalty_gradient_shape, data_path, plot_path, iteration):
     params=np.loadtxt(os.path.join(data_path, "Parameters", f"Param{iteration}.txt"))
-    gradients_penalty = np.loadtxt(os.path.join(data_path, "Gradients_Penalty", f"Gradient{iteration}.txt"))
+    gradients_penalty = np.loadtxt(os.path.join(data_path, "Gradient_Penalties", f"Gradient{iteration}.txt"))
     plt.plot(x, penalty_gradient_shape)
     plt.plot(params, gradients_penalty, 'o')
     #plt.legend(loc='lower right')
-    plt.title('Shape of Gradient of Penalty')
+    plt.title(f'Gradient of Penalty at Iteration {iteration}')
     plt.ylabel('Gradient')
     plt.xlabel('Pixel Value')
     plt.rc('axes', titlesize=14)     # fontsize of the axes title
@@ -75,11 +86,21 @@ def plotPenaltyGradients(data_path, plot_path, iteration):
     plt.savefig(os.path.join(plot_path, "Gradients_Penalty", f"penaltyGradient_{iteration}.png"), bbox_inches='tight')
     plt.close()
 
-def plotParabolic(data_path, plot_path, info_dict):
+def plotPenaltyShape(penalty_type, data_path, plot_path, info_dict):
+    match penalty_type:
+        case 'parabolic':
+            titleinfo = ""
+        case 'gaussian':
+            sigma = info_dict["sigma"]
+            mu = info_dict["mu"]
+            titleinfo = f"Width={sigma}, Center at {mu}"
+        case 'triangular':
+            slope = info_dict["slope"]
+            titleinfo = f"Slope={slope}"
+
     x, penalty_shape=np.loadtxt(os.path.join(data_path, "Penalty_Shape.txt"), delimiter=',', unpack=True)
     plt.plot(x, penalty_shape)
-    #plt.legend(loc='lower right')
-    plt.title('Parabolic Penalty')
+    plt.title(f'{penalty_type} Penalty, ' + titleinfo)
     plt.ylabel('Penalty')
     plt.xlabel('Pixel Value')
     plt.rc('axes', titlesize=14)     # fontsize of the axes title
@@ -89,8 +110,7 @@ def plotParabolic(data_path, plot_path, info_dict):
 
     x, penalty_gradient_shape=np.loadtxt(os.path.join(data_path, "PenaltyGradient_Shape.txt"), delimiter=',', unpack=True)
     plt.plot(x, penalty_gradient_shape)
-    #plt.legend(loc='lower right')
-    plt.title('Gradient of Parabolic Penalty')
+    plt.title(f'Gradient of {penalty_type} Penalty, ' + titleinfo)
     plt.ylabel('Gradient')
     plt.xlabel('Pixel Value')
     plt.rc('axes', titlesize=14)     # fontsize of the axes title
@@ -98,56 +118,7 @@ def plotParabolic(data_path, plot_path, info_dict):
     plt.savefig(os.path.join(plot_path, "penaltyGradient_shape.png"), bbox_inches='tight')
     plt.close()
 
-def plotGaussian(data_path, plot_path, info_dict):
-    sigma = info_dict["sigma"]
-    mu = info_dict["mu"]
-    x, penalty_shape=np.loadtxt(os.path.join(data_path, "Penalty_Shape.txt"), delimiter=',', unpack=True)
-    plt.plot(x, penalty_shape)
-    #plt.legend(loc='lower right')
-    plt.title(f'Gaussian Penalty, Width={sigma}, Center at {mu}')
-    plt.ylabel('Penalty')
-    plt.xlabel('Pixel Value')
-    plt.rc('axes', titlesize=14)     # fontsize of the axes title
-    plt.rc('axes', labelsize=12)    # fontsize of the x and y labels
-    plt.savefig(os.path.join(plot_path, "penalty_shape.png"), bbox_inches='tight')
-    plt.close()
-
-    x, penalty_gradient_shape=np.loadtxt(os.path.join(data_path, "PenaltyGradient_Shape.txt"), delimiter=',', unpack=True)
-    plt.plot(x, penalty_gradient_shape)
-    #plt.legend(loc='lower right')
-    plt.title(f'Gradient of Gaussian Penalty, Width={sigma}, Center at {mu}')
-    plt.ylabel('Gradient')
-    plt.xlabel('Pixel Value')
-    plt.rc('axes', titlesize=14)     # fontsize of the axes title
-    plt.rc('axes', labelsize=12)    # fontsize of the x and y labels
-    plt.savefig(os.path.join(plot_path, "penaltyGradient_shape.png"), bbox_inches='tight')
-    plt.close()
-
-def plotTriangular(data_path, plot_path, info_dict):
-    slope = info_dict["slope"]
-    x, penalty_shape=np.loadtxt(os.path.join(data_path, "Penalty_Shape.txt"), delimiter=',', unpack=True)
-    plt.plot(x, penalty_shape)
-    #plt.legend(loc='lower right')
-    plt.title(f'Triangular Penalty, Slope={slope}')
-    plt.ylabel('Penalty')
-    plt.xlabel('Pixel Value')
-    plt.rc('axes', titlesize=14)     # fontsize of the axes title
-    plt.rc('axes', labelsize=12)    # fontsize of the x and y labels
-    plt.savefig(os.path.join(plot_path, "penalty_shape.png"), bbox_inches='tight')
-    plt.close()
-
-    x, penalty_gradient_shape=np.loadtxt(os.path.join(data_path, "PenaltyGradient_Shape.txt"), delimiter=',', unpack=True)
-    plt.plot(x, penalty_gradient_shape)
-    #plt.legend(loc='lower right')
-    plt.title(f'Gradient of Triangular Penalty, Slope={slope}')
-    plt.ylabel('Gradient')
-    plt.xlabel('Pixel Value')
-    plt.rc('axes', titlesize=14)     # fontsize of the axes title
-    plt.rc('axes', labelsize=12)    # fontsize of the x and y labels
-    plt.savefig(os.path.join(plot_path, "penaltyGradient_shape.png"), bbox_inches='tight')
-    plt.close()
-
-def Shape(geometry,diel,d,angle1=225,angle2=45,colormax=1,shapebarrier=0.5,plotDpi=100,iteration=-1,position="./",FullLattice=False):
+def plotGeometry(geometry,diel,d,angle1=225,angle2=45,colormax=1,shapebarrier=0.5,plotDpi=100,iteration=-1,position="./",FullLattice=False):
     """Plot the shape of object as dot matrix.
     #Input:
     # --SC                                                         SolutionClass

python_bindings/dda_model/binarizers.py
@@ -20,26 +20,16 @@ def gradGaussian(parameters, info_dict):
 
 def triangular(parameters, info_dict):
     slope = info_dict["slope"]
-    result = np.array(parameters)
-
-    for i, para in enumerate(parameters):
-        if(para <= 0.5):
-            result[i] = slope*para
-        else:
-            result[i] = slope*(1-para)
-
+    result = np.array(parameters) * slope
+    indices = np.where(parameters > 0.5)
+    result[indices] = slope - result[indices]
     return result
 
 def gradTriangular(parameters, info_dict):
     slope = info_dict["slope"]
-    result = np.array(parameters)
-
-    for i, para in enumerate(parameters):
-        if(para <= 0.5):
-            result[i] = slope
-        else:
-            result[i] = -slope
-
+    result = np.ones_like(parameters) * slope
+    indices = np.where(parameters > 0.5)
+    result[indices] *= -1
     return result
 
 def piecewise_update_absolute(iter, info_dict):

python_bindings/dda_model/optimizers.py
@@ -22,6 +22,7 @@ class AdamOptimizer:
 
         return result
     
+# NOTE: Implementation does not seem to match one that Ben linked in PR (May 2024)
 class AdaGradOptimizer:
     def __init__(self):
         self.squaredGradient = 0
@@ -30,6 +31,6 @@ class AdaGradOptimizer:
 
         result = np.array(gradients)
         print("Using Adagrad Optimizer.")
-        self.squaredGradient += np.power(gradients, 2) / 100000
+        self.squaredGradient += np.power(gradients, 2)
         result = gradients / (self.squaredGradient + 1)
         return result